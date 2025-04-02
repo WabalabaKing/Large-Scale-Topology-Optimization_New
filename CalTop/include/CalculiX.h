fb1b5a3a349ce723522268af09ae9d01f1f36847
Prateek Ranjan <prateekr@mit.edu>
2025-04-02 03:02:50 -0400

    set passive rho =1

--- CalTop/include/CalculiX.h
+++ CalTop/include/CalculiX.h
@@ -110,6 +110,8 @@ void write_volume_sensitivities(int ne,
 
 int* passiveElements(char *filename, int *numPassive);
 
+void filterOutPassiveElems_density(double *design, int ne, int *passiveIDs, int numPassive);
+
 void rho(double *design,int ne);
 
 void tecplot_vtu(int nk, int ne, double *co, int *kon, int *ipkon, double *v, double *stx, double *rhoPhy);
---
+++ CalTop/src/filterPassiveElements.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * Sets the design variable to 0.0 for passive elements.
+ *
+ * @param design        Pointer to the density array (length = ne).
+ * @param ne            Number of elements in the mesh.
+ * @param passiveIDs    Array of passive element IDs (1-based).
+ * @param numPassive    Number of passive element IDs.
+ */
+void filterOutPassiveElems_density(double *design, int ne, int *passiveIDs, int numPassive) 
+{
+    for (int i = 0; i < numPassive; i++) 
+    {
+        int eid = passiveIDs[i];  // 1-based ID
+
+        if (eid >= 1 && eid <= ne) 
+        {
+            design[eid - 1] = 1.0;  // Convert to 0-based index
+        } 
+        else 
+        {
+            printf("Warning: Passive element ID %d is out of bounds [1, %d]\n", eid, ne);
+        }
+    }
+}
