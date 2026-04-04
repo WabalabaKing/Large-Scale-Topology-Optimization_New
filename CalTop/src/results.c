2548eb998ae1eae03769cf67ab75aa4156025794
WabalabaKing <wz10@illinois.edu>
2026-04-04 01:44:42 -0400

    Reapply "now nonlingeo is officially more stable"
    This reverts commit ecf8a1eac9f94d62f1b5655a8943b6008be2870c.

--- CalTop/src/results.c
+++ CalTop/src/results.c
@@ -834,7 +834,7 @@ void *resultsmechmt(ITG *i)
           springarea1,reltime1,&calcul_fn1,&calcul_qa1,&calcul_cauchy1,nener1,
 	  &ikin1,&nal[indexnal],ne01,thicke1,emeini1,
 	  pslavsurf1,pmastsurf1,mortar1,clearini1,&nea,&neb,ielprop1,prop1,
-	  kscale1,&list1,ilist1));
+	  kscale1,&list1,ilist1,design1,penal1,rhomin1));
 
     
 
