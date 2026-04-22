4eb0bb90853b2f77199b4a2297afec1528b36cc4
Prateek Ranjan <prateekr@mit.edu>
2026-04-22 14:19:30 -0500

    fix FEA struct seg fault

--- PreCICE/precice-2.2.1/src/xml/ConfigParser.cpp
+++ PreCICE/precice-2.2.1/src/xml/ConfigParser.cpp
@@ -82,7 +82,7 @@ void OnCharacters(void *ctx, const xmlChar *ch, int len)
   pParser->OnTextSection(std::string(reinterpret_cast<const char *>(ch), len));
 }
 
-void OnStructuredErrorFunc(void *userData, xmlError *error)
+void OnStructuredErrorFunc(void *userData, const xmlError *error)
 {
   const std::string message{error->message};
 
