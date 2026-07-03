d066ceaba09d8e9f311331a059a45be5ec0cb88c
Prateek Ranjan <prateekr@mit.edu>
2026-07-03 00:47:45 -0500

    def fullSolidVol_sum

--- CalTop/src/output/write_objectives.c
+++ CalTop/src/output/write_objectives.c
@@ -29,11 +29,11 @@ void write_objectives(int ne,
 {
     const char *filename = "objectives.csv";
 
-    /* total material volume with rho = 1 */
+    /* total material volume with rho = 1 a.k.a FULL SOLID VOLUME */
     double initialVol_sum = 0;
 
-    /* initial design volume of active domain */
-    double initialDesignVol_sum = 0.0;
+    /* Volume for full solud domain */
+    double fullSolidVol_sum = 0.0;
 
     /* total material volume with optimized rho */
     double  designVol_sum = 0;
@@ -41,7 +41,7 @@ void write_objectives(int ne,
     /* design domain discreteness */
     double discreteness_sum = 0.0;
 
-    /* Check for old sensitivity file and delete it */
+    /* Check for old objectives.csv and delete it */
     if (access(filename, F_OK) == 0)
     {
         if (remove(filename) != 0)
@@ -51,16 +51,19 @@ void write_objectives(int ne,
         }
     }
 
-    /* Open new sensitivity file */
+    /* Open new objectives.csv */
     FILE *obj_file = fopen(filename, "w");
-    if (!obj_file) {
+    if (!obj_file) 
+    {
         perror("Error opening objectives.csv");
         exit(EXIT_FAILURE);
     }
 
     /* NOTE:
-        ORIGINAL VOLUME := Volume of the structure with rho = 1 
+        FULL SOLID VOLUME := Volume of the structure with rho = 1 
         DESIGN VOLUME   := Volume of the structure with filtered rho
+
+        -> When passive elements present, both FULL SOLID VOUME and DESIGN VOLUME should exclude the volume of passive elements.
     */
 
 
@@ -84,23 +87,22 @@ void write_objectives(int ne,
 
     int nthreads = omp_get_max_threads();
 
-    printf("Using %d CPU(s) for volume and discreteness evaluation\n",
-       nthreads);
+    printf("Using %d CPU(s) for volume and discreteness evaluation\n", nthreads);
     fflush(stdout);
 
 
-
     /* Write file header */
-    fprintf(obj_file, "COMPLIANCE, ORIGINAL VOLUME, DESIGN VOLUME, VOLUME_FRACTION, DISCRETENESS, MASS, CGx, CGy, CGz, PNORM\n");
+    fprintf(obj_file, "COMPLIANCE, FULL SOLID VOLUME, DESIGN VOLUME, VOLUME_FRACTION, DISCRETENESS, MASS, CGx, CGy, CGz, PNORM\n");
 
-    #pragma omp parallel for reduction(+:initialVol_sum,initialDesignVol_sum,designVol_sum,discreteness_sum)
+    #pragma omp parallel for reduction(+:initialVol_sum,fullSolidVol_sum,designVol_sum,discreteness_sum)
     for (int i = 0; i < ne; i++)
     {
-        initialVol_sum += eleVol[i];
+        initialVol_sum += eleVol[i];  // Note: This is not used anywhere
         discreteness_sum += rhoPhys[i] * (1.0 - rhoPhys[i]);
-
+         
+        /* skip passive elements */
         if (!is_skin[i])
-        {   initialDesignVol_sum+= eleVol[i];
+        {   fullSolidVol_sum+= eleVol[i];
             designVol_sum += eleVol[i] * rhoPhys[i];
         }
     }
@@ -108,16 +110,16 @@ void write_objectives(int ne,
     /* Compute volume fraction for active domain only */
     double volume_fraction = 0.0;
     
-    if (initialDesignVol_sum > 0.0)
+    if (fullSolidVol_sum > 0.0)
     {
-        volume_fraction = designVol_sum / initialDesignVol_sum;
+        volume_fraction = designVol_sum / fullSolidVol_sum;
     }
 
     /* Compute discreteness */
     double discreteness = (4.0 /ne) * discreteness_sum;
 
     /* Write structure compliance and volume to file */
-    fprintf(obj_file, "%.15f, %.15f, %.15f, %.15f, %.15f, %.15f, %.15f, %.15f, %.15f, %.15f \n", *compliance_sum, initialVol_sum, designVol_sum, volume_fraction, discreteness, *Mass, *cgx, *cgy, *cgz, *pnorm);
+    fprintf(obj_file, "%.15f, %.15f, %.15f, %.15f, %.15f, %.15f, %.15f, %.15f, %.15f, %.15f \n", *compliance_sum, fullSolidVol_sum, designVol_sum, volume_fraction, discreteness, *Mass, *cgx, *cgy, *cgz, *pnorm);
     
     fclose(obj_file);
 
