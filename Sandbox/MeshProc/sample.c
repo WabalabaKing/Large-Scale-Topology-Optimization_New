11b24697b463350aa3196f13302818d9a91e6390
Prateek Ranjan <prateekr@mit.edu>
2026-07-05 22:29:22 -0500

    init file searcher

--- Sandbox/MeshProc/sample.c
+++ Sandbox/MeshProc/sample.c
@@ -1,3 +1,37 @@
 // A sample c-script to convert native su2 mesh to CalTop format
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <math.h>
+
+#define MAXLINE 512
+
+void find_su2_file(char *filename)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(".");
+
+    if (dir == NULL)
+    {
+        printf("ERROR: Cannot open current directory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    
+
+
+}
+
+
+int main (void)
+{
+    /* find su2 file in current directory*/
+
+
+}
+
