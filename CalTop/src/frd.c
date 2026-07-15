d11178c19e6faeee9e4ec80feae596c5bb3dede0
Prateek Ranjan <prateekr@mit.edu>
2026-07-15 17:07:57 -0500

    add remove eigenfreq flags

--- CalTop/src/frd.c
+++ CalTop/src/frd.c
@@ -189,7 +189,7 @@ void frd(double *co,ITG *nk,ITG *kon,ITG *ipkon,char *lakon,ITG *ne0,
     fprintf(f1,"%5sUHOST                                                              \n",p1);
     fprintf(f1,"%5sUPGM               CalculiX                                        \n",p1);
     fprintf(f1,"%5sUVERSION           Version 2.15                             \n",p1);
-    fprintf(f1,"%5sUCOMPILETIME       Mon Jul 13 04:08:14 CDT 2026                    \n",p1);
+    fprintf(f1,"%5sUCOMPILETIME       Wed Jul 15 17:07:11 CDT 2026                    \n",p1);
     fprintf(f1,"%5sUDIR                                                               \n",p1);
     fprintf(f1,"%5sUDBN                                                               \n",p1);
     
